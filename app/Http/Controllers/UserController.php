741862422ff24549513328c1641e6e35871963c6
thivalls <olavalls@gmail.com>
2020-12-23 12:44:52 -0300

    Preparing code for the class

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function resource()
+    {
+        //
+    }
+
+    public function show(User $user)
+    {
+        //
+    }
+}
